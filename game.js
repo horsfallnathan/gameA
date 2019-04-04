29f5f94fa86f3b94056acacd97822bee8a89674d
mjarraya <mjarraya@student.42.fr>
2019-04-04 20:12:28 +0200

    fixed collision for away player

--- game.js
+++ game.js
@@ -1,38 +1,42 @@
 class Game {
-    constructor() {
-        this.pitch = new Pitch();
-        this.awayPlayer = new Player(800, 400, 65, 68, 87, 83, 32, 'home');
-        this.player = new Player(400, 400, 37, 39, 38, 40, 76, 'away');
-        this.homePost = new Posts(0, GAME_HEIGHT / 2, 40, 180);
-        this.awayPost = new Posts(GAME_WIDTH, GAME_HEIGHT / 2, 40, 180);
-        this.goalie = new Goalkeeper(GAME_WIDTH, GAME_HEIGHT / 2, GAME_WIDTH, 'away');
-        this.homeGoalie = new Goalkeeper(0, GAME_HEIGHT / 2, 'home');
-        this.ball = new Ball();
-        this.post = new Posts();
-    }
+  constructor() {
+    this.pitch = new Pitch();
+    this.awayPlayer = new Player(800, 400, 65, 68, 87, 83, 32, 'away');
+    this.homePlayer = new Player(400, 400, 37, 39, 38, 40, 76, 'home');
+    this.homePost = new Posts(0, GAME_HEIGHT / 2, 40, 180);
+    this.awayPost = new Posts(GAME_WIDTH, GAME_HEIGHT / 2, 40, 180);
+    this.goalie = new Goalkeeper(
+      GAME_WIDTH,
+      GAME_HEIGHT / 2,
+      GAME_WIDTH,
+      'away'
+    );
+    this.homeGoalie = new Goalkeeper(0, GAME_HEIGHT / 2, 'home');
+    this.ball = new Ball();
+    this.post = new Posts();
+  }
 
-    setup() {
-        createCanvas(GAME_WIDTH, GAME_HEIGHT);
-        frameRate(60);
-        this.ball.setup();
-        this.pitch.setup();
-        this.player.setup();
-        this.awayPlayer.setup();
-        this.player.move();
-        this.homePost.setup();
-        this.awayPost.setup();
-        this.goalie.setup();
-        this.homeGoalie.setup();
-    }
-    draw() {
-        background(0, 180, 0);
-        this.pitch.draw();
-        this.goalie.draw();
-        this.homeGoalie.draw();
-        this.player.draw();
-        this.awayPlayer.draw();
-        this.ball.draw();
-        this.homePost.draw();
-        this.awayPost.draw();
-    }
+  setup() {
+    createCanvas(GAME_WIDTH, GAME_HEIGHT);
+    frameRate(60);
+    this.ball.setup();
+    this.pitch.setup();
+    this.awayPlayer.setup();
+    this.homePlayer.setup();
+    this.homePost.setup();
+    this.awayPost.setup();
+    this.goalie.setup();
+    this.homeGoalie.setup();
+  }
+  draw() {
+    background(0, 180, 0);
+    this.pitch.draw();
+    this.goalie.draw();
+    this.homeGoalie.draw();
+    this.awayPlayer.draw();
+    this.homePlayer.draw();
+    this.ball.draw();
+    this.homePost.draw();
+    this.awayPost.draw();
+  }
 }
